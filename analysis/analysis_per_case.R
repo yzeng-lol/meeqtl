b2faa74bd9294beacd77e61e8f8446e50c82cf3d
Yong <Yong@He-301C.local>
2021-03-17 12:32:57 -0400

    delete_ana

--- analysis/analysis_per_case.R
+++
@@ -1,8 +0,0 @@
-
-# this script will analysis each case -------------------------------------
-
-probeid <- "chr8:23415796"
-probegr <- GRanges(Rle("chr8"), IRanges(23415796, width = 1, names = probeid))
-liftOver_wrapper(probegr, convertFrom = "Hg38", chain = "~/Documents/DB/hg38ToHg19.over.chain")
-
-plot_blood_cor(probeid, outfile = paste0("plots/blood_",probeid,".pdf"))
